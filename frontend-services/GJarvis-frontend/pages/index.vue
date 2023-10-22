1702aacf661e14cda1c9516d1fd5b2dfbfcc9c3a
n.morishita <javastudy23@gmail.com>
2023-10-22 22:02:48 +0900

    メニュー追加

--- frontend-services/GJarvis-frontend/pages/index.vue
+++ frontend-services/GJarvis-frontend/pages/index.vue
@@ -1,29 +1,61 @@
 <template>
-    <div>
-        <v-toolbar dark prominent image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-container class="ma-0">
+    <v-row>
+      <v-col class="pl-0">
+          <v-layout>
+            <v-navigation-drawer>
+              <v-list>
+                <v-list-item title="Jarvis" subtitle="Welcome to this page">
+                </v-list-item>
+              </v-list>
 
-            <v-toolbar-title>Vuetify</v-toolbar-title>
+              <v-divider></v-divider>
 
-            <v-spacer></v-spacer>
+              <v-list :lines="false" density="compact" nav>
+                <v-list-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  :value="item"
+                  color="blue"
+                >
+                  <template v-slot:prepend>
+                    <v-icon :icon="item.icon"></v-icon>
+                  </template>
 
-            <v-btn icon>
-                <v-icon>mdi-export</v-icon>
-            </v-btn>
-        </v-toolbar>
-    </div>
-    <div>
-        <h1>aaa{{ count }}</h1>
-        <h1>12312321312</h1>
-    </div>
-    <v-sheet :height="200" :width="200"></v-sheet>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-navigation-drawer>
+
+            <v-main style="height: 354px"></v-main>
+          </v-layout>
+      </v-col>
+      <v-col :cols="9">
+        <div>
+          <h1>aaa{{ count }}</h1>
+          <h1>12312321312</h1>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-const { data: count } = await useFetch('/api/v1/voice-assistance-service/hello')
+import { ref } from "vue";
+const items = ref([
+  { text: "My Files", icon: "mdi-folder" },
+  { text: "Shared with me", icon: "mdi-account-multiple" },
+  { text: "Starred", icon: "mdi-star" },
+  { text: "Recent", icon: "mdi-history" },
+  { text: "Offline", icon: "mdi-check-circle" },
+  { text: "Uploads", icon: "mdi-upload" },
+  { text: "Backups", icon: "mdi-cloud-upload" },
+]);
+
+const { data: count } = await useFetch(
+  "/api/v1/voice-assistance-service/hello"
+);
 </script>
-
-
 
 <!-- <script lang="ts" setup>
 import { ref, onMounted } from "vue";
