8baf785255a3a09911456e02be8e7157543cae92
n_morishita_gxp <n.morishita@gxp.co.jp>
2023-10-27 22:05:34 +0900

    displayを変更

--- frontend-services/GJarvis-frontend/pages/index.vue
+++ frontend-services/GJarvis-frontend/pages/index.vue
@@ -6,7 +6,7 @@
         </v-list-item>
       </v-list>
 
-      <v-divider></v-divider>
+      <v-divider style="color: white;"></v-divider>
 
       <v-list :lines="false" density="compact" nav>
         <v-list-item
@@ -24,7 +24,23 @@
     </v-navigation-drawer>
 
     <v-main>
-      <v-container fluid> text </v-container>
+      <v-container fluid>
+        <v-row
+          v-for="(msg, index) in mergeMsgs"
+          :key="index"
+          :class="msg.class">
+          <v-spacer />
+          <v-col cols="1" class="d-flex justify-center align-center">
+            <v-icon :icon="msg.icon"></v-icon>
+          </v-col>
+          <v-col cols="7">
+            <v-card min-height="50" variant="tonal">{{
+              msg.msg
+            }}</v-card>
+          </v-col>
+          <v-spacer />
+        </v-row>
+      </v-container>
     </v-main>
     <v-footer app
       ><v-row class="d-flex align-center">
@@ -32,10 +48,18 @@
           <v-btn class="rounded-circle" icon="mdi-microphone"></v-btn>
         </v-col>
         <v-col cols="10">
-          <v-textarea rows="1" hide-details="auto"></v-textarea>
+          <v-textarea
+            rows="1"
+            hide-details="auto"
+            v-model="newUserMsg"
+            placeholder="メッセージを送信"
+            style="background: white;"></v-textarea>
         </v-col>
         <v-col cols="1">
-          <v-btn class="rounded-circle" icon="mdi-send"></v-btn>
+          <v-btn
+            class="rounded-circle"
+            icon="mdi-send"
+            @click="sendMsg(newUserMsg)"></v-btn>
         </v-col>
       </v-row>
     </v-footer>
@@ -53,7 +77,43 @@ const items = ref([
   { text: "Uploads", icon: "mdi-upload" },
   { text: "Backups", icon: "mdi-cloud-upload" },
 ]);
+
+const newUserMsg = ref("");
+const mergeMsgs = ref([]);
+
+const sendMsg = (msg) => {
+  if (newUserMsg.value == "") {
+    return;
+  }
+  mergeMsgs.value.push({ icon: "mdi-account", msg: msg, class: "user" });
+  const newResMsg = "Your Message is 「" + msg + "」";
+  mergeMsgs.value.push({
+    icon: "$vuetify",
+    msg: newResMsg,
+    class: "llama",
+  });
+  newUserMsg.value = "";
+};
 </script>
+<style scoped>
+.v-navigation-drawer {
+  color: white;
+  background: rgb(36,36,36);
+}
+.v-main, .v-footer {
+  background: rgb(111, 116, 129);
+}
+
+.user {
+  color: white;
+  background: rgb(36, 36, 36);
+}
+
+.llama {
+  color: white;
+  background: rgb(111, 116, 129);
+}
+</style>
 
 <!-- <script lang="ts" setup>
 import { ref, onMounted } from "vue";
